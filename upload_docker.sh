8d87d01b07046bd45eea9f91570522d4bd1ee415
kabba <danielmuthama23@gmail.com>
2023-02-06 16:23:29 +0300

    update files

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,14 +5,11 @@
 
 # Step 1:
 # Create dockerpath
-dockerpath=newthingade/flaskapp
+# dockerpath=<your docker ID/path>
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
-docker login
-docker tag flaskapp $dockerpath
 
 # Step 3:
 # Push image to a docker repository
-docker push $dockerpath
